e46d87bfc91a72575daa9b0959f7a0a47ca38e09
Stephanie Schneider <sschneider713@gmail.com>
2013-06-28 12:10:15 -0400

    update

--- test/timeline/timeline.spec.js
+++ test/timeline/timeline.spec.js
@@ -1,68 +1,23 @@
-datas = [''];	
+var datas = [1387959625000,1353282133000,1344038600000,1340092813000,1364656027000,1374508754000,1369841743000,
+1355628308000,1400766482000,1369750279000,1384007456000,1374537060000,1389253430000,1348952488000,1345639741000,
+1360665027000,1386572893000,1367971646000,1386992323000,1396551843000,1386486489000,1375609067000,1353890516000,
+1353894116000,1353894176000,1376947286000,1355568202000,1347417294000,1363393012000,1370126240000,1346421623000];	
 
 
-describe('Test src/widgets/rawFeed/js/table.js base functions', function(){
-
-	it('Tests extractData function for correct lower limiting', function() {
-		var startDate = Date.parse('2012-09-04T01:00:00-08:00'); //before second event
-		var endDate = new Date();
-
-		var result = extractData(startDate, endDate);
-		
-		expect(result.length).toBe(3)
-	});
-
-	it('Tests extractData function for correct upper limiting', function() {
-		var startDate = new Date(0);
-		var endDate = Date.parse('2012-09-05T01:00:00-08:00'); //after second event;
-
-		var result = extractData(startDate, endDate);
-		
-		expect(result.length).toBe(2);
-	});
-
-	it('Tests extractData function for correct upper and lowe limiting', function() {
-		var startDate = Date.parse('2012-09-04T01:00:00-08:00'); //before second event;
-		var endDate = Date.parse('2012-09-05T01:00:00-08:00'); //after second event;
-
-		var result = extractData(startDate, endDate);
-		
-		expect(result.length).toBe(1);
-	});
-
-	it('Tests createTable', function() {
-		var startDate = Date.parse('2012-09-04T01:00:00-08:00'); //before second event;
-		var endDate = Date.parse('2012-09-05T01:00:00-08:00'); //after second event;
-
-		extractData = jasmine.createSpy('extractData').andCallFake(function(start, end) {
-			return [{
-				"time": "2000-11-04T17:42:56-08:00",
-				"ent1": "his co-conspirators",
-				"rel": "devise",
-				"ent2": "a scheme"
-			}];
+describe('Test src/widgets/timeline/js/testing.js base functions', function(){
+	
+	describe('Tests the add function', function(){
+	
+		it('Tests add method call logic', function(){
+			spyOn(d3, 'selectAll').andCallThrough();
+			spyOn(d3, 'select').andCallThrough();
+	
+			add();
+	
+			//if select and selectAll were called with correct params
+			expect(d3.selectAll).toHaveBeenCalledWith('th');
+			expect(d3.select).toHaveBeenCalledWith('#submit');
+			expect(d3.select).toHaveBeenCalledWith('#reset');
 		});
-
-		spyOn(window, 'TableView').andCallFake(function(x) {
-			return {};
-		});
-		
-		var result = createTable(startDate, endDate);
-
-		expect(result).toBeDefined();
-
-		expect(TableView).toHaveBeenCalled();
-
-		expect(extractData).toHaveBeenCalled();
-	});
-
-	it('Tests createClickers method call logic', function(){
-		spyOn(d3, 'selectAll').andCallThrough();
-		spyOn(d3, 'select').andCallThrough();
-
-		createClickers();
-
-		expect(d3.selectAll).toHaveBeenCalledWith('th');
-		expect(d3.select).toHaveBeenCalledWith('#submit');
 	});
 });
