0ae229e5a25075c017c9c0f0cea69018b87f4a28
Evan Nehring <evan.d.nehring@gmail.com>
2013-07-02 17:10:54 -0400

    Add mouseover events for easier viewing of data

--- src/widgets/heatChart/js/month.js
+++ src/widgets/heatChart/js/month.js
@@ -5,11 +5,19 @@ var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
 var chart = circularHeatChart()
 	.range(["white", "red"])
 	.segmentLabels(months)
-	.segmentHeight(11)
+	.segmentHeight(10)
 	.numSegments(12)
 	.innerRadius(10);
 
-for(var i = 0; i < 372; i++) data[i] = Math.random();
+for(var i = 0; i < 372; i++){
+
+	var month = i % 12;
+	var day = Math.floor((i / 12) % 31);
+
+	data[i] = {title: day + 1 + " of " + months[month], value: Math.random()};
+}
+
+chart.accessor(function(d) {return d.value});
 
 d3.select('#monthChart')
 	.selectAll('svg')
@@ -17,3 +25,13 @@ d3.select('#monthChart')
 	.enter()
 	.append('svg')
 	.call(chart);
+
+d3.selectAll("#monthChart path").on('mouseover', function(){
+	var d = d3.select(this).data()[0];
+	d3.select("#monthInfo").text(d.title + ' has value ' + d.value);
+});
+
+d3.selectAll("#monthChart svg").on('mouseout', function(){
+	var d = d3.select(this).data()[0];
+	d3.select("#monthInfo").text('');
+});
