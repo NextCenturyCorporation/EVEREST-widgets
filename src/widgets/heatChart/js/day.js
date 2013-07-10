c5d60de85d23e195653749c4ecdd90fa130def24
Evan Nehring <evan.d.nehring@gmail.com>
2013-07-10 15:21:11 -0400

    Fix problems with asynchronous calls in week.js and month.js (much like the changes done with day.js)

--- src/widgets/heatChart/js/day.js
+++ src/widgets/heatChart/js/day.js
@@ -6,85 +6,84 @@ var HOURS_PER_DAY = 24;
 var day_heatChart_widget = {};
 
 day_heatChart_widget.execute = function() {
-    var raw_data = [MINUTES_PER_DAY];
-    
-    for (var i = 0; i < MINUTES_PER_DAY; i++){
-    	raw_data[i] = 0;
-    }
-    
-    owfdojo.addOnLoad(function(){
-    	OWF.ready(function(){
-    		OWF.Eventing.subscribe("testChannel1", function(sender, msg){
-    			var fields_start = msg.split("[");
-    			var fields_end = fields_start[1].split("]");
-    			var fields = fields_end[0];
-    			var data = fields.split(",");
+	var hours = ['12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am',
+		'9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm',
+		'7pm', '8pm', '9pm', '10pm', '11pm'];
+	var day_chunks= [];
+	var raw_data = [MINUTES_PER_DAY];
 
-    			for (var j = 0; j < data.length; j++){
-    				var time = new Date(parseInt(data[j]));
-                                var hours = time.getHours();
+	for (var i = 0; i < MINUTES_PER_DAY; i++){
+		raw_data[i] = 0;
+	}
 
-    				raw_data[time.getHours() + (HOURS_PER_DAY * time.getMinutes())] += 1;
+	owfdojo.addOnLoad(function(){
+		OWF.ready(function(){
+			OWF.Eventing.subscribe("testChannel1", function(sender, msg){
+				// Get an array of all date objects
+				var fields_start = msg.split("[");
+				var fields_end = fields_start[1].split("]");
+				var fields = fields_end[0];
+				var data = fields.split(",");
 
-    			}
+				for (var j = 0; j < data.length; j++){
+					var time = new Date(parseInt(data[j]));
+					var hours = time.getHours();
 
-                        var heatChart_chunks = [];
-                        var hours = ['12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am',
-                        '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm',
-                        '7pm', '8pm', '9pm', '10pm', '11pm'];
+					// This will map the number of raw feeds for a specific date to the correct heat chart "chunk"
+					raw_data[time.getHours() + (HOURS_PER_DAY * time.getMinutes())] += 1;
 
-                        var chart = circularHeatChart()
-                            .range(["white", "blue"])
-                            .segmentLabels(hours)
-                            .segmentHeight(4.7)
-                            .innerRadius(10);
+				}
 
-                        for(var k = 0; k < MINUTES_PER_DAY; k++){
+				var chart = circularHeatChart()
+				.range(["white", "blue"])
+				.segmentLabels(hours)
+				.segmentHeight(4.7)
+				.innerRadius(10);
 
-                            var hour = k % HOURS_PER_DAY;
-                            var minutes = Math.floor((k / HOURS_PER_DAY) % MINUTES_PER_HOUR);
-                            var meridiem = "am";
+				for(var k = 0; k < MINUTES_PER_DAY; k++){
 
-                            if(hour === 0) {
-                                hour = 12;
-                            }
-                            else if(hour === 12){
-                                meridiem = "pm";
-                            }
-                            else if(hour > 12){
-                                hour = hour - 12;
-                                meridiem = "pm";
-                            }
+					var hour = k % HOURS_PER_DAY;
+					var minutes = Math.floor((k / HOURS_PER_DAY) % MINUTES_PER_HOUR);
+					var meridiem = "am";
 
-                            if(minutes < 10) {
-                                minutes = "0" + minutes;
-                            }
+					if(hour === 0) {
+						hour = 12;
+					}
+					else if(hour === 12){
+						meridiem = "pm";
+					}
+					else if(hour > 12){
+						hour = hour - 12;
+						meridiem = "pm";
+					}
 
-                            heatChart_chunks[k] = {title: hour + ":" + minutes + " " + meridiem,
-                                value: raw_data[k]};
-                        }
+					if(minutes < 10) {
+						minutes = "0" + minutes;
+					}
 
+					day_chunks[k] = {title: hour + ":" + minutes + " " + meridiem,
+						value: raw_data[k]};
+				}
 
-                        chart.accessor(function(d) {return d.value});
+				chart.accessor(function(d) {return d.value});
 
-                        d3.select('#dayChart')
-                            .selectAll('svg')
-                            .data([heatChart_chunks])
-                            .enter()
-                            .append('svg')
-                            .call(chart);
+				d3.select('#dayChart')
+					.selectAll('svg')
+					.data([day_chunks])
+					.enter()
+					.append('svg')
+					.call(chart);
 
-                        d3.selectAll("#dayChart path").on('mouseover', function(){
-                                var d = d3.select(this).data()[0];
-                                d3.select("#dayInfo").text(d.value + ' raw feed(s) added at  ' + d.title);
-                                });
+				d3.selectAll("#dayChart path").on('mouseover', function(){
+					var d = d3.select(this).data()[0];
+					d3.select("#dayInfo").text(d.value + ' raw feed(s) added at  ' + d.title);
+				});
 
-                        d3.selectAll("#dayChart svg").on('mouseout', function(){
-                                //var d = d3.select(this).data()[0];
-                                d3.select("#dayInfo").text('');
-                                });
-                });
-        });
-    });
-
+				d3.selectAll("#dayChart svg").on('mouseout', function(){
+					//var d = d3.select(this).data()[0];
+					d3.select("#dayInfo").text('');
+				});
+			});
+		});
+	});
 };
--- src/widgets/heatChart/js/week.js
+++ src/widgets/heatChart/js/week.js
@@ -1,55 +1,82 @@
+var HOURS_PER_WEEK = 168;
+var DAYS_PER_WEEK = 7;
+
 var week_heatChart_widget = {};
 
 week_heatChart_widget.execute = function() {
-    var data = [];
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
-        'Saturday'];
+	var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+		'Saturday'];
+	var raw_data = [HOURS_PER_WEEK];
+	var week_chunks = []
 
-    var chart = circularHeatChart()
-        .range(["white", "green"])
-        .segmentLabels(days)
-        .segmentHeight(12)
-        .innerRadius(10)
-        .numSegments(7);
+		for (var i = 0; i < HOURS_PER_WEEK; i++) {
+			raw_data[i] = 0;
+		}
 
-    for(var i = 0; i < 168; i++){
+	owfdojo.addOnLoad(function() {
+		OWF.ready(function() {
+			OWF.Eventing.subscribe("testChannel1", function(sender, msg) {
+				// Get an array of all date objects
+				var fields_start = msg.split("[");
+				var fields_end = fields_start[1].split("]");
+				var fields = fields_end[0];
+				var data = fields.split(",");
 
-        var day = i % 7;
-        var hour = Math.floor((i / 7) % 24);
-        var meridiem = "am";
+				for (var j = 0; j < data.length; j++) {
+					var time = new Date(parseInt(data[j]));
 
-        if(hour === 0) {
-            hour = 12;
-        }
-        else if(hour === 12) {
-            meridiem = "pm";
-        }
-        else if(hour > 12) {
-            hour = hour - 12;
-            meridiem = "pm";
-        }
+					// This will map the number of raw feeds for a specific date to the correct heat chart "chunk"
+					raw_data[time.getDay() + (DAYS_PER_WEEK * time.getHours())] += 1;
+				}
 
-        data[i] = {title: days[day] + ", " + hour + " " + meridiem,
-            value: Math.random()};
-    }
+				var chart = circularHeatChart()
+				.range(["white", "green"])
+				.segmentLabels(days)
+				.segmentHeight(12)
+				.innerRadius(10)
+				.numSegments(7);
 
-    chart.accessor(function(d) {return d.value});
+				for(var i = 0; i < 168; i++){
 
-    d3.select('#weekChart')
-        .selectAll('svg')
-        .data([data])
-        .enter()
-        .append('svg')
-        .call(chart);
+					var day = i % 7;
+					var hour = Math.floor((i / 7) % 24);
+					var meridiem = "am";
 
-    d3.selectAll("#weekChart path").on('mouseover', function(){
-            var d = d3.select(this).data()[0];
-            d3.select("#weekInfo").text(d.title + ' has value ' + d.value);
-            });
+					if(hour === 0) {
+						hour = 12;
+					}
+					else if(hour === 12) {
+						meridiem = "pm";
+					}
+					else if(hour > 12) {
+						hour = hour - 12;
+						meridiem = "pm";
+					}
 
-    d3.selectAll("#weekChart svg").on('mouseout', function(){
-            var d = d3.select(this).data()[0];
-            d3.select("#weekInfo").text('');
-            });
+					week_chunks[i] = {title: days[day] + ", " + hour + " " + meridiem,
+						value: raw_data[i]};
+				}
 
+				chart.accessor(function(d) {return d.value});
+
+				d3.select('#weekChart')
+					.selectAll('svg')
+					.data([week_chunks])
+					.enter()
+					.append('svg')
+					.call(chart);
+
+				d3.selectAll("#weekChart path").on('mouseover', function(){
+					var d = d3.select(this).data()[0];
+					d3.select("#weekInfo").text(d.value + ' raw feed(s) added on ' + d.title);
+				});
+
+				d3.selectAll("#weekChart svg").on('mouseout', function(){
+					var d = d3.select(this).data()[0];
+					d3.select("#weekInfo").text('');
+				});
+
+			});
+		});
+	});
 }
