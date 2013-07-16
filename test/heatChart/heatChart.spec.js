0d6b5eb711c1bcec57946a6f04ad29d38a125078
Evan Nehring <evan.d.nehring@gmail.com>
2013-07-16 11:01:10 -0400

    Set up skeleton for jasmine tests

--- test/heatChart/heatChart.spec.js
+++ test/heatChart/heatChart.spec.js
@@ -1,1 +1,13 @@
 // jasmine tests for the heat chart widget
+
+describe('Test src/widget/heatChart/js/day.js', function() {
+
+});
+
+describe('Test src/widget/heatChart/js/week.js', function() {
+
+});
+
+describe('Test src/widget/heatChart/js/month.js', function() {
+
+});
