af787b8ca1e6d503ecb0793da2f032b5dccdc130
Evan Nehring <evan.d.nehring@gmail.com>
2013-07-16 15:53:06 -0400

    Create jasmine tests to make sure the heat chart is being created for day, week, and month

--- test/heatChart/heatChart.spec.js
+++ test/heatChart/heatChart.spec.js
@@ -1,13 +1,40 @@
 // jasmine tests for the heat chart widget
 
+// Test for the day heat chart
 describe('Test src/widget/heatChart/js/day.js', function() {
 
+	describe('Tests the OWF subscribe function', function() {
+
+		it('', function() {
+			var result = day_heatChart_widget.execute();
+
+			expect(chart).toBeDefined();
+		});
+	});
 });
 
+// Test for the week heat chart
 describe('Test src/widget/heatChart/js/week.js', function() {
 
+	describe('Tests the OWF subscribe function', function() {
+
+		it('', function() {
+			var result = day_heatChart_widget.execute();
+
+			expect(chart).toBeDefined();
+		});
+	});
 });
 
+// Test for the month heat chart
 describe('Test src/widget/heatChart/js/month.js', function() {
 
+	describe('Tests the OWF subscribe function', function() {
+
+		it('', function() {
+			var result = day_heatChart_widget.execute();
+
+			expect(chart).toBeDefined();
+		});
+	});
 });
