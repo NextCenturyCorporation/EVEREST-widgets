da693f95b424881fd45828eac7ed328389810330
Evan Nehring <evan.d.nehring@gmail.com>
2013-07-17 15:26:08 -0400

    Add tests for the execute() methods

--- test/heatChart/heatChart.spec.js
+++ test/heatChart/heatChart.spec.js
@@ -3,12 +3,27 @@
 // Test for the day heat chart
 describe('Test src/widget/heatChart/js/day.js', function() {
 
-	describe('Tests the OWF subscribe function', function() {
+	describe('Test day_heatChart_widget methods', function() {
+		it('Test to see if day_heatChart_widget is defined', function() {
+			expect(day_heatChart_widget).toBeDefined();
 
-		it('', function() {
-			var result = day_heatChart_widget.execute();
+			var date_list = ['2012-09-04T01:00:00-08:00'];
 
-			expect(chart).toBeDefined();
+			day_heatChart_widget.execute = jasmine.createSpy('day_heatChart_widget').andCallFake(function() {
+
+				var hour_labels = ['12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am',
+				'9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm',
+				'7pm', '8pm', '9pm', '10pm', '11pm'];
+			var raw_data = [MINUTES_PER_DAY];
+			var day_chunks= [];
+
+			day_heatChart_widget.createChart(date_list, raw_data, day_chunks, hour_labels);
+
+			});
+
+			var result = day_heatChart_widget.execute;
+
+			expect(result).toBeDefined();
 		});
 	});
 });
@@ -16,12 +31,26 @@ describe('Test src/widget/heatChart/js/day.js', function() {
 // Test for the week heat chart
 describe('Test src/widget/heatChart/js/week.js', function() {
 
-	describe('Tests the OWF subscribe function', function() {
+	describe('Test week_heatChart_widget methods', function() {
 
-		it('', function() {
-			var result = day_heatChart_widget.execute();
+		it('Test to see if week_heatChart_widget is defined', function() {
+			expect(week_heatChart_widget).toBeDefined();
 
-			expect(chart).toBeDefined();
+			var date_list = ['2012-09-04T01:00:00-08:00'];
+
+			week_heatChart_widget.execute = jasmine.createSpy('week_heatChart_widget').andCallFake(function() {
+
+				var day_labels = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+				var raw_data = [HOURS_PER_WEEK];
+				var week_chunks= [];
+
+				day_heatChart_widget.createChart(date_list, raw_data, week_chunks, day_labels);
+
+			});
+
+			var result = week_heatChart_widget.execute;
+
+			expect(result).toBeDefined();
 		});
 	});
 });
@@ -29,12 +58,26 @@ describe('Test src/widget/heatChart/js/week.js', function() {
 // Test for the month heat chart
 describe('Test src/widget/heatChart/js/month.js', function() {
 
-	describe('Tests the OWF subscribe function', function() {
+	describe('Test month_heatChart_widget methods', function() {
 
-		it('', function() {
-			var result = day_heatChart_widget.execute();
+		it('Test to see if month_heatChart_widget is defined', function() {
+			expect(month_heatChart_widget).toBeDefined();
 
-			expect(chart).toBeDefined();
+			var date_list = ['2012-09-04T01:00:00-08:00'];
+
+			week_heatChart_widget.execute = jasmine.createSpy('week_heatChart_widget').andCallFake(function() {
+
+				var month_labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+				var raw_data = [HOURS_PER_WEEK];
+				var week_chunks= [];
+
+				day_heatChart_widget.createChart(date_list, raw_data, week_chunks, day_labels);
+
+			});
+
+			var result = week_heatChart_widget.execute;
+
+			expect(result).toBeDefined();
 		});
 	});
 });
