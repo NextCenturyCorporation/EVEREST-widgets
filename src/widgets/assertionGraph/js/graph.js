b13caae8db223a6f16122a14c2bb5a22a86b21bb
Evan Nehring <evan.d.nehring@gmail.com>
2013-07-26 09:24:03 -0400

    Fix window dimensions

--- src/widgets/assertionGraph/js/graph.js
+++ src/widgets/assertionGraph/js/graph.js
@@ -2,7 +2,7 @@
 // and http://stackoverflow.com/questions/8663844/add-text-label-onto-links-in-d3-force-directed-graph
 
 
-var width = 1200,
+var width = 700,
 	height = 700;
 
 var color = d3.scale.category10();
