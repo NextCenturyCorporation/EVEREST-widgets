803d79adb8846f95dc781bb0f24e1c419dd97de2
Evan Nehring <evan.d.nehring@gmail.com>
2013-07-26 17:01:33 -0400

    Add jasmine test functions

--- test/assertionGraph/asseritonGraph.spec.js
+++ test/assertionGraph/asseritonGraph.spec.js
@@ -1,3 +1,42 @@
 /**
  * Assertion Graph Jasmine Test File
  */
+
+/**
+ * Test what happens during a normal run.
+ */
+describe('Test src/widget/assertionGraph/js/convert.js', function() {
+
+	describe('Test the createArray() function', function() {
+		it('for proper parsing behavior', function() {
+
+			var data = [{
+				"entity1": "dog",
+				"relationship": "caught",
+				"entity2": "ball"
+			}];
+
+			createArrays(data);
+
+			expect(nodes).toBeDefined();
+
+		});
+
+	});
+});
+
+describe('Test src/widget/assertionGraph/js/graph.js', function() {
+
+});
+
+/**
+ * Test what happens if one of the json entries is missing an entity or
+ * relationship.
+ */
+describe('Test src/widget/assertionGraph/js/convert.js', function() {
+
+});
+
+describe('Test src/widget/assertionGraph/js/graph.js', function() {
+
+});
