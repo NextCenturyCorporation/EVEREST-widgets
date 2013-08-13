0de3a93301bb9bb02a181db21933cf25669fe793
Ashley <aviendha2281@gmail.com>
2013-08-13 15:45:34 -0400

    updating so both panes appear in line

--- src/widgets/confirmReports/js/confirmer.js
+++ src/widgets/confirmReports/js/confirmer.js
@@ -3,11 +3,6 @@ var confirmer = function(){
 	var url = 'http://everest-build:8081/';
 	me.alpha_reports = [];
 	me.assertions = [];
-	
-	//holder	
-	me.createCanvas = function(){
-		d3.select('.target-pattern').append('svg');
-	};
 
 	me.createListeners = function(){
 		d3.select('.confirm').on('click', function(){
@@ -24,11 +19,14 @@ var confirmer = function(){
 			var elem = $(this)[0];
 			var elem_id = elem.options[elem.selectedIndex].text;
 			
-			for (var i = 0; i < me.assertions.length; i++){
-				if (me.assertions[i]._id === elem_id){
-					d3.selectAll('text').remove();
-					d3.select('.alpha-info').append('text')
-						.text(JSON.stringify(me.assertions[i]));
+			
+			for (var i = 0; i < me.alpha_reports.length; i++){
+				if (me.alpha_reports[i]._id === elem_id){
+					d3.selectAll('.alpha-text').remove();
+					d3.select('.alpha-info').append('div')
+						.attr('class', 'alpha-text')
+							.append('text')
+							.text(JSON.stringify(me.alpha_reports[i]));
 				}
 			}
 			me.getAssertions(elem_id);
@@ -68,6 +66,7 @@ var confirmer = function(){
 	
 	me.getAssertions = function(ar_id){
 		$.getJSON(url +'assertion/?callback=?', function(asserts){
+			me.assertions = asserts;
 			var assertions = [];
 			for (var i = 0; i < asserts.length; i++){
 				if (asserts[i].alpha_report_id === ar_id){
@@ -76,26 +75,39 @@ var confirmer = function(){
 			}
 			
 			d3.select('.assertion-graph').remove();
-			if (assertions.length !== 0){
-				var svg = d3.select('.asserts').append('svg')
-					.attr('class', 'assertion-graph')
-					.attr('width', 490)
-					.attr('height', 490);
-					
-				svg.append('defs').append('marker')
-					.attr('id', 'Triangle')
-					.attr('refX', 0).attr('refY', 3)
-					.attr('markerUnits', 'strokeWidth')
-					.attr('markerWidth', 100)
-					.attr('markerHeight', 100)
-					.attr('orient', 'auto')
-					.append('svg:path')
-						.attr('d', 'M 0 0 L 6 3 L 0 6 z');
-						
+			d3.select('.target-graph').remove();
+			
+			var svg = d3.select('.asserts').append('svg')
+				.attr('class', 'assertion-graph')
+				.attr('width', 490)
+				.attr('height', 490);
+				
+			svg.append('defs').append('marker')
+				.attr('id', 'Triangle')
+				.attr('refX', 0).attr('refY', 3)
+				.attr('markerUnits', 'strokeWidth')
+				.attr('markerWidth', 100)
+				.attr('markerHeight', 100)
+				.attr('orient', 'auto')
+				.append('svg:path')
+					.attr('d', 'M 0 0 L 6 3 L 0 6 z');
+			
+			var svg1 = d3.select('.target-pattern').append('svg')
+				.attr('class', 'target-graph')
+				.attr('width', 490)
+				.attr('height', 490);
+				
+			if (assertions.length !== 0){			
 				//display assertions
 				var net = new network(svg, assertions, 'disjoint');
 				net.draw();
 				net.draw({}, { entity1: "A", relationship: "E", entity2: "B" });
+				
+				//display assertions
+				var net1 = new network(svg1, assertions);
+				net1.draw();
+				net1.draw({}, { entity1: "A", relationship: "E", entity2: "B" });
+				net1.draw({}, { entity1: "C", relationship: "E", entity2: "B" });
 			} 
 		});
 	};
@@ -127,8 +139,11 @@ var confirmer = function(){
 			var ar = me.alpha_reports[0];
 			
 			//display alpha report
-			d3.select('.alpha-info').append('text')
-				.text(JSON.stringify(ar));
+			d3.selectAll('.alpha-text').remove();
+			d3.select('.alpha-info').append('div')
+				.attr('class', 'alpha-text')
+					.append('text')
+					.text(JSON.stringify(ar));
 			
 			me.getAssertions(ar._id);
 		}, 2000);
