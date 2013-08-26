7ecd6d155612a142678e7c8242062d0ed070ff63
Josh Williams <halfs13@gmail.com>
2013-08-26 13:33:02 -0400

    Adding php post relay

---
+++ lib/post_relay.php
@@ -0,0 +1,30 @@
+<?php
+//$data_string = http_get_request_body();
+$data_string = file_get_contents('php://input');
+
+$data_json = json_decode($data_string);
+
+$data = $data_json->{'data'};
+$url = $data_json->{'url'};
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+	CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => $url,
+ 	CURLOPT_POST => 1,
+ 	CURLOPT_POSTFIELDS => $data
+));
+
+$resp = curl_exec($curl);
+//print_r($resp);
+$resp_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+
+HttpResponse::status($resp_code);
+HttpResponse::setContentType('application/json');
+HttpResponse::setData($resp);
+HttpResponse::send();
+
+curl_close($curl);
+?>
