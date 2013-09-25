91c57735ad9ff2d69feb29fb15f79336e8a7bca8
Ashley <aviendha2281@gmail.com>
2013-09-25 15:14:53 -0400

    assertionGraph dir

---
+++ src/components/assertionGraph/assertionGraph/convert.js
@@ -0,0 +1,150 @@
+/** Converts a json objedt which contains the following key/value pairs:
+ * {
+ * 		"entity1": "false claims",
+ * 		"relationship": "aggravate",
+ * 		"entity2": "identity theft"
+ * }
+ *
+ * into two arrays, nodes and links, the proper format to use the force d3
+ * example:
+ * 
+ * nodes = [
+ * 		{"name": "false claims", "group":0},
+ * 		{"name": "identity theft", "group":1},
+ * 		{"name": "the core group of defendants", "group":1}
+ * ];
+ *
+ * links = [
+ * 		{"source":0, "target":1, "value":"aggravate"},
+ *		{"source":2, "target":3, "value":"file"},
+ *		{"source":4, "target":5, "value":"expand"},
+ * ];
+ *
+ * where the number after source and target point to the index location in
+ * the nodes array of the node to point to for this link
+ */
+var count = 0;
+var entity1Color = '#333399';
+var entity2Color = '#339966';
+var bothColor = '#9900cc';
+
+function getCircle(item, array){
+	var cs = [];
+	for (var i = 0; i < array.length; i++){
+		if (item.value.toLowerCase() === array[i].value){
+			cs.push(array[i]);
+		}
+	}
+	return cs;
+}
+
+function indexOfCircle(item, array, disjoint){
+	for (var i = 0; i < array.length; i++){
+		if (disjoint){
+			if (item.value === array[i].value 
+					&& item.ent === array[i].ent
+					&& item.group === array[i].group){
+				return i;
+			}
+		} else {
+			if (item.value === array[i].value 
+					&& item.ent === array[i].ent){
+				return i;
+			}
+		}
+	}
+	return -1;
+}
+
+function indexOfLink(item, array){
+	for (var i = 0; i < array.length; i++){
+		if (item.value === array[i].value && item.source === array[i].source.index
+				&& item.target === array[i].target.index){
+			return i;
+		}
+	}
+	return -1;
+}
+
+function indexOfMessage(item, links, circles){	
+	for (var i = 0; i < links.length; i++){
+		var a = links[i];
+		if (typeof(a.source) === 'object'){
+			if (a.source.value === item.entity1.toLowerCase() 
+					&& a.target.value == item.entity2.toLowerCase()
+					&& a.value === item.relationship.toLowerCase()){
+				return i;
+			}
+		} else {
+			if (circles[a.source].value === item.entity1.toLowerCase()
+				 	&& circles[a.target].value === item.entity2.toLowerCase()
+				 	&& a.value === item.relationship.toLowerCase()){
+				return i;
+			}
+		}
+	}
+	return -1;
+}
+
+function addNewAssertion(nodes, links, msg, disjoint){
+	var ent1 = {};
+	var ent2 = {};
+	var nodes1, nodes2;
+	if(indexOfMessage(msg, links, nodes) === -1){
+		ent1 = { 
+			value: msg.entity1.toLowerCase(),
+			color: entity1Color,
+			group: count
+		};
+				
+		ent2 = { 
+			value: msg.entity2.toLowerCase(),
+			color: entity2Color,
+			group: count+1
+		};
+		
+		var nodes1 = getCircle(ent1, nodes);
+		
+		if(nodes1.length === 0){
+			nodes.push(ent1);
+			count++;
+		} else if (disjoint){
+			nodes.push(ent1);
+			count++;
+		} else {			
+			if (nodes1[0].color !== ent1.color){
+				nodes1[0].color = bothColor;
+				ent1.color = bothColor;
+			}
+		}
+			
+		var nodes2 = getCircle(ent2, nodes);
+		
+		if(nodes2.length === 0){
+			nodes.push(ent2);
+			count++;
+		} else if (disjoint){
+			nodes.push(ent2);
+			count++;
+		} else {			
+			if (nodes2[0].color !== ent2.color){
+				nodes2[0].color = bothColor;
+				ent2.color = bothColor;
+			}
+		}
+	
+		var index1 = indexOfCircle(ent1, nodes, disjoint);
+		var index2 = indexOfCircle(ent2, nodes, disjoint);
+	
+		var rel = {
+			source: index1,
+			target: index2,
+			value: msg.relationship.toLowerCase()
+		};
+	
+		if (indexOfLink(rel, links) === -1){
+			links.push(rel);
+		}
+	}
+	return [nodes, links];
+}
---
+++ src/components/assertionGraph/assertionGraph/network.js
@@ -0,0 +1,153 @@
+// Code based on http://bl.ocks.org/mbostock/2706022
+// wimbledon.prcweb.co.uk/davidgoliath.html
+// and http://stackoverflow.com/questions/8663844/add-text-label-onto-links-in-d3-force-directed-graph
+function midpoint(p1, p2){
+	return {
+		x: (p1.x + p2.x) / 2,
+		y: (p1.y + p2.y) / 2
+	};
+};
+
+var network = function(svg, data, disjoint){
+	var me = this;
+	me.svg = svg;		
+	me.svg.append('defs').append('marker')
+		.attr('id', 'Triangle')
+		.attr('refX', 0).attr('refY', 3)
+		.attr('markerUnits', 'strokeWidth')
+		.attr('markerWidth', 100)
+		.attr('markerHeight', 100)
+		.attr('orient', 'auto')
+		.append('svg:path')
+			.attr('d', 'M 0 0 L 6 3 L 0 6 z');
+	
+	me.nodes = [];
+	me.links = [];
+	for (var i = 0; i < data.length; i++){
+		me.arrays = addNewAssertion(me.nodes, me.links, data[i], disjoint);
+		me.nodes = me.arrays[0];
+		me.links = me.arrays[1];
+	}
+	
+	me.link = svg.selectAll('.link');
+	me.node = svg.selectAll('.node');
+	me.color = d3.scale.category10();
+	me.linktext;
+	me.radius = 8;
+	
+	me.force = d3.layout.force()
+		.size([svg.attr('width'), svg.attr('height')])
+		.linkDistance(100)
+		.charge(-500);
+		
+	me.draw = function(sender, msg){
+		if ( msg ){
+			var arrays = addNewAssertion(me.nodes, me.links, msg, disjoint);
+			me.nodes = arrays[0];
+			me.links = arrays[1];
+		}
+		
+		me.force
+			.nodes(me.nodes)
+			.links(me.links)
+			.on("tick", me.tick)
+			.start();
+		
+		me.link = me.link.data(me.links);
+		me.link.exit().remove();
+		
+		me.link.enter().insert("path", ":first-child")
+			.attr("class", "link")
+			.attr('marker-mid', 'url(#Triangle)');
+		
+		me.node = me.node.data(me.nodes)
+		me.node.exit().remove();
+		
+		var nodeEnter = me.node.enter().append("g")
+			.attr("class", "node")
+			.on("mouseover", me.mouseover)
+			.on("mouseout", me.mouseout)
+			.style("fill", function(d){
+				return d.color;
+			})
+			.call(me.force.drag);
+				
+		me.linktext = me.svg.selectAll(".linklabel").data(me.force.links());
+		me.linktext.enter()
+			.append("text")
+			.attr("class", "linklabel")
+			.attr("dx", 1)
+			.attr("dy", "1em")
+			.attr("text-anchor", "middle")
+			.text(function(d) { 
+				if (d._label !== undefined){
+					return d._label; 
+				} else {
+					return d.value;
+				}
+			});
+		
+		nodeEnter.append("circle")
+			.attr('class', function(d) { return d.value; })
+			.attr("r", me.radius);
+		
+		nodeEnter.append("text")
+			.attr("x", 12)
+			.attr("dy", ".35em")
+			.text(function(d) { 
+				if (d.name !== undefined){
+					return d.name; 
+				} else {
+					return d.value;
+				}
+			});
+		
+		//d3.selectAll('circle').each(function(){		changes those in the other graphs too	
+		if(!disjoint){
+			svg.selectAll('circle').each(function(){
+				var c = d3.select(this);
+				for (var i = 0; i < me.nodes.length; i++){
+					if (c.attr('class') === me.nodes[i].value){
+						c.style('fill', function(d){
+							return d.color;
+						});
+					}
+				}
+			});
+		}
+	};
+	
+	me.tick = function(){
+		me.link.attr('d', function(d){
+			var mid = midpoint(d.source, d.target);
+			return 'M'+d.source.x+' '+d.source.y+' L'+mid.x+' '+mid.y+' L'+d.target.x+' '+d.target.y;
+		});
+		
+		me.node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+	
+		me.linktext.attr("transform", function(d) {
+			return "translate(" + (d.source.x + d.target.x) / 2 + "," 
+					+ (d.source.y + d.target.y) / 2 + ")"; 
+		});
+	};
+	
+	me.mouseover = function(){
+		d3.select(this).select('circle').transition()
+			.duration(750)
+			.attr("r", 2 * me.radius);
+	};
+	
+	me.mouseout = function(){
+		d3.select(this).select('circle').transition()
+			.duration(750)
+			.attr("r", me.radius);
+	};
+	
+	me.setNodes = function(n){
+		me.nodes = n;
+	};
+	
+	me.setLinks = function(l){
+		me.links = l;
+	};
+};
