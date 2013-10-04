e7887c0ff6111c1aec71ce671d4008b92afddce2
bnewsom <benjamin.newsom@gmail.com>
2013-10-04 13:13:17 -0400

    clean up unused vars

--- src/widgets/confirmReports/js/target_event_view.js
+++ src/widgets/confirmReports/js/target_event_view.js
@@ -134,7 +134,9 @@ var target_event_view = function(svg){
 			.on('mouseover', me.mouseover)
 			.on('mouseout', me.mouseout);
 
-		var path = me.createArrow(line);
+//		var path = me.createArrow(line);  // path var is never used
+		
+		me.createArrow(line);
 		
 		if(indexOfObj(me.lines, l._id, '_id') === -1){
 			var lObj = {
