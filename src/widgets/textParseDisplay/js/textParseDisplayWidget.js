c76414935f510bffb05f67eea99a854c7caa4867
Josh Williams <halfs13@gmail.com>
2013-10-18 13:03:24 -0400

    Including second tab code for parse result widget

--- src/widgets/textParseDisplay/js/textParseDisplayWidget.js
+++ src/widgets/textParseDisplay/js/textParseDisplayWidget.js
@@ -1,5 +1,6 @@
 var textParseDisplayWidget = {
-	execute: function(alphaReportChannel) {
+	execute: function() {
+		var alphaReportChannel = "com.nextcentury.everest.data_table_announce.alpha-report";
 		var textParseDisplayObject = new textParseDisplay();
 
 		textParseDisplayObject.execute();
