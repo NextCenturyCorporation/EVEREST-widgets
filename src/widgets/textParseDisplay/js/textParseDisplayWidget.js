693af0a823108be8dbb9f7f7a32c6a295f751618
Josh Williams <halfs13@gmail.com>
2013-10-18 13:20:56 -0400

    Trying to fix eventing

--- src/widgets/textParseDisplay/js/textParseDisplayWidget.js
+++ src/widgets/textParseDisplay/js/textParseDisplayWidget.js
@@ -1,6 +1,5 @@
 var textParseDisplayWidget = {
 	execute: function() {
-		var alphaReportChannel = "com.nextcentury.everest.data_table_announce.alpha-report";
 		var textParseDisplayObject = new textParseDisplay();
 
 		textParseDisplayObject.execute();
