57e420e8362b1e823afb1fd4d2d98b9fb2ba6044
Ashley <aviendha2281@gmail.com>
2013-10-21 14:27:24 -0400

    updating confirmReports to appear as similar.js in titan-test and comparing against subset when item is selected

---
+++ src/widgets/confirmReports/js/buildQuery.js
@@ -0,0 +1,191 @@
+var titanAddress = 'http://everest-build:8182/graphs/graph';
+
+/**
+	GET
+*/
+var getAllEdges = function(){
+	return titanAddress + '/tp/gremlin?script=g.E';
+};
+
+var getAllVertices = function(){
+	return titanAddress + '/tp/gremlin?script=g.V';
+};
+
+var getEdgeById = function(id){
+	return titanAddress + '/edges/' + id;
+};
+
+var getVertexById = function(id){
+	return titanAddress + '/vertices/' + id;
+};
+
+var getGroupById = function(id){
+	return titanAddress+'/tp/gremlin?script=g.v(' + id + ').inE.outV';
+};
+
+var getVertexNamesById = function(id){
+	return titanAddress+'/tp/gremlin?script=g.v(' + id + ').inE.outV.name';
+};
+
+var getEdgeLabelsById = function(id){
+	return titanAddress+'/tp/gremlin?script=g.v(' + id + ').inE.outV.inE.label';
+};
+
+var getGroupPathById = function(id){
+	return titanAddress+'/tp/gremlin?script=g.v(' + id + ').inE.outV.outE.inV.path';
+};
+
+var getGroupVertexCountById = function(id){
+	return titanAddress+'/tp/gremlin?script=g.v(' + id + ').inE.outV.count()';
+};
+
+var getGroupEdgeCountById = function(id){
+	return titanAddress+'/tp/gremlin?script=g.v(' + id + ').inE.outV.inE.count()';
+};
+
+var getAssertionsById = function(id){
+	return titanAddress+'/tp/gremlin?script=g.v(' + id + ').inE.outV.inE.outV.path';
+};
+
+var getMatchingVertices = function(id, array){
+	var query = titanAddress+'/tp/gremlin?script=g.v(' + id + ').inE.outV.or(';
+	array.forEach(function(d){
+		query += '_().has("name","' + d + '"),';
+		
+	});
+	if ( query.charAt(query.length - 1) === ','){
+		query = query.substring(0, query.length - 1);
+	}
+	query += ')';
+	query = query.replace(/\#/g, '');
+	query = query.replace(/\+/g, '');
+	query = query.replace(/\\/g, '');
+	console.log(query);
+	return query;
+};
+
+var getMatchingEdges = function(id, array){
+	var query = titanAddress+'/tp/gremlin?script=g.v(' + id + ').inE.outV.inE.or(';
+	array.forEach(function(d){
+		query += '_().has("label","' + d + '"),';
+	});
+	
+	if ( query.charAt(query.length - 1) === ','){
+		query = query.substring(0, query.length - 1);
+	}
+	query += ')';
+	query = query.replace(/\#/g, '');
+	query = query.replace(/\+/g, '');
+	query = query.replace(/\\/g, '');
+	console.log(query);
+	return query;
+};
+
+var getMatchingOrientation = function(id, array){
+	var query = titanAddress+'/tp/gremlin?script=g.v(' + id + ').inE.outV.or(';
+	array.forEach(function(d){
+		query += '_().has("name","' + d[0].name + 
+			'").inE.has("label","' + d[1]._label + 
+			'").outV.has("name","' + d[2].name + '"),';
+	});
+	if ( query.charAt(query.length - 1) === ','){
+		query = query.substring(0, query.length - 1);
+	}
+	query += ')';
+	query = query.replace(/\#/g, '');
+	query = query.replace(/\+/g, '');
+	query = query.replace(/\\/g, '');
+	return query;
+};
+
+var buildKeyValueCountQuery = function(key, value){
+	var query = titanAddress+'/tp/gremlin?script=g.V.has("' + key + '","' + value + '").count()';
+	query = query.replace(/\#/g, '');
+	query = query.replace(/\+/g, '');
+	query = query.replace(/\\/g, '');
+	return query;
+};
+
+var buildKeyValueQuery = function(key, value, start, end){
+	var query = titanAddress+'/tp/gremlin?script=g.V.has("' + key + '","' + value + '")';
+	
+	if ( value !== 'alpha report' && value !== 'target event'){
+		query += '.outE.has("label","metadata of").inV';
+	}
+	
+	if (start !== undefined && end !== undefined){
+		query += '[' + start + '..' + end + ']';
+	}
+	query = query.replace(/\#/g, '');
+	query = query.replace(/\+/g, '');
+	query = query.replace(/\\/g, '');
+	console.log(query);
+	return query;
+};
+
+var getMetadataVertex = function(id){
+	return titanAddress+'/tp/gremlin?script=g.v(' + id + ').outE.has("label","metadata of").inV';
+};
+
+
+/**
+	POST / Create
+*/
+var buildEdge = function(lObj){
+	var outV = lObj.source;
+	var inV = lObj.target;
+	var query = titanAddress + '/edges';
+	if ( lObj._titan_id !== undefined ) {
+		query += '/' + lObj._titan_id;
+	}
+	
+	query += '?_outV=' + outV + '&_inV=' + inV + '&';
+	var keys = Object.keys(lObj);
+	keys.forEach(function(key){
+		if (key !== 'html' && key !== '_titan_id' && key !== 'source' && key !== 'target'){
+			if (key === 'd'){
+				query += '_label=' + lObj[key];
+			} else {
+				query += key + '=' + lObj[key];
+			}
+			
+			query += '&';
+		}
+	});
+	query = query.replace(/\#/g, '');
+	query = query.replace(/\+/g, '');
+	query = query.replace(/\\/g, '');
+	return query;
+};
+
+var buildNode = function(cObj){
+	var query = titanAddress + '/vertices';
+	
+	if (cObj._titan_id !== undefined ) {
+		query += '/' + cObj._titan_id;
+	}
+	query += '?';
+	var keys = Object.keys(cObj);
+	keys.forEach(function(key){
+		if (key !== 'html' && key !== '_titan_id' && key !== 'x' && key !== 'y'){
+			if (key === 'd'){
+				query += 'name=' + cObj[key];
+			} else {
+				query += key + '=' + cObj[key];
+			}
+			
+			query += '&';
+		}
+	});
+	query = query.replace(/\#/g, '');
+	query = query.replace(/\+/g, '');
+	query = query.replace(/\\/g, '');
+	return query;
+};
+
+/**
+	POST / Update
+*/
+var updateVertexQuery = function(id, obj){
+	return titanAddress + '/vertices/' + id + '?comparedTo=' + JSON.stringify(obj);
+};
---
+++ src/widgets/confirmReports/js/compare.js
@@ -0,0 +1,353 @@
+var indexOfObj = function(ra, value, attribute){
+	for (var i = 0; i < ra.length; i++){
+		if (value.toLowerCase() === ra[i][attribute].toLowerCase()){
+			return i;
+		}
+	}
+	
+	return -1;
+};
+
+var getUnique = function(array){
+	var new_array = [];
+	array.forEach(function(d){
+		if (new_array.indexOf(d) === -1){
+			new_array.push(d);
+		}
+	});
+	return new_array;
+};
+
+var getUniqueObjectArray = function(array){
+	var new_array = [];
+	array.forEach(function(d){
+		if (indexOfObj(new_array, d.name, 'name') === -1){
+			new_array.push(d);
+		}
+	});
+	return new_array;
+};
+
+var scores = {};
+
+var compareVertexAmount = function(id1, id2){
+	$.ajax({
+		type: 'GET',
+		url: getGroupVertexCountById(id1),
+		dataType: 'application/json',
+		success: function(r){ 
+			//console.log('success');
+		},
+		error: function(e){
+			var ar_length = JSON.parse(e.responseText).results[0];	
+			//console.log("The Left Graph vertex length: " + ar_length);				
+			$.ajax({
+				type: 'GET',
+				url: getGroupVertexCountById(id2),
+				dataType: 'application/json',
+				success: function(r){ 
+					//console.log('success');
+				},
+				error: function(e){
+					var te_length = JSON.parse(e.responseText).results[0];
+					//console.log("The Right Graph vertex length: " + te_length);
+					
+					if (te_length === ar_length){
+						$(scores[id1]).find('.true').append('<li>There are the same number of vertices</li>');
+					} else {
+						$(scores[id1]).find('.false').append('<li>There are a different number of vertices</li>');
+					}
+				}
+			});
+		}
+	});
+};
+
+var compareEdgeAmount = function(id1, id2){
+	$.ajax({
+		type: 'GET',
+		url: getGroupEdgeCountById(id1),
+		dataType: 'application/json',
+		success: function(r1){ 
+			//console.log('success');
+		},
+		error: function(e){
+			var ar_length = JSON.parse(e.responseText).results[0];
+			//console.log("The Left Graph edge length: " + ar_length);			
+			$.ajax({
+				type: 'GET',
+				url: getGroupEdgeCountById(id2),
+				dataType: 'application/json',
+				success: function(r){ 
+					//console.log('success');
+				},
+				error: function(e){
+					var te_length = JSON.parse(e.responseText).results[0];
+					//console.log("The Right Graph edge length: " + te_length);
+					if (te_length === ar_length){
+						$(scores[id1]).find('.true').append('<li>There are the same number of edges</li>');
+					} else {
+						$(scores[id1]).find('.false').append('<li>There are a different number of edges</li>');
+					}
+				}
+			});
+		}
+	});
+};
+
+//second ajax query unnecessary if following a match vertex length
+var compareVertices = function(id1, id2){
+	$.ajax({
+		type: 'GET',
+		url: getVertexNamesById(id1),
+		dataType: 'application/json',
+		success: function(r1){ 
+			//console.log('success');
+		},
+		error: function(e){
+			var ar_names = JSON.parse(e.responseText).results;
+			var unique_ar = getUnique(ar_names);
+			
+			//console.log("The Left Graph vertex names: ");
+			//console.log(ar_names);
+			$.ajax({
+				type: 'GET',
+				url: getMatchingVertices(id2, ar_names),
+				dataType: 'application/json',
+				success: function(r){ 
+					//console.log('success');
+				},
+				error: function(e){
+					var te_matches = JSON.parse(e.responseText).results;
+					var unique_te = getUniqueObjectArray(te_matches);
+					//console.log("The Right Graph vertex matches: ");
+					//console.log(te_matches);
+					if (unique_te.length === unique_ar.length){
+						$(scores[id1]).find('.true').append('<li>The Left Graph vertices are subset of The Right Graph</li>');
+					//} else if (te_matches.length > 0) {
+					//	$(scores[id1]).find('.true').append('<li>Some of The Left Graph vertices match those of The Right Graph</li>');
+					} else {
+						$(scores[id1]).find('.false').append('<li>The Left Graph vertices are not subset of The Right Graph</li>');
+						//$(scores[id1]).find('.false').append('<li>None of The Left Graph vertices match those of The Right Graph</li>');
+					}
+				}
+			});
+		}
+	});
+	
+	$.ajax({
+		type: 'GET',
+		url: getVertexNamesById(id2),
+		dataType: 'application/json',
+		success: function(r1){ 
+			//console.log('success');
+		},
+		error: function(e){
+			var te_names = JSON.parse(e.responseText).results;
+			var unique_te = getUnique(te_names);
+			//console.log("The Right Graph vertex names: ");
+			//console.log(te_names);
+			
+			$.ajax({
+				type: 'GET',
+				url: getMatchingVertices(id1, te_names),
+				dataType: 'application/json',
+				success: function(r){ 
+					//console.log('success');
+				},
+				error: function(e){
+					var ar_matches = JSON.parse(e.responseText).results;
+					var unique_ar = getUniqueObjectArray(ar_matches);
+					//console.log("The Left Graph vertex matches: ");
+					//console.log(ar_matches);
+					if (unique_ar.length === unique_te.length){
+						$(scores[id1]).find('.true').append('<li>The Right Graph vertices are subset of The Left Graph</li>');
+					//} else if (ar_matches.length > 0 ){
+					//	$(scores[id1]).find('.true').append('<li>Some of The Right Graph vertices match those of The Left Graph</li>');
+					} else {
+						$(scores[id1]).find('.false').append('<li>The Right Graph vertices are not subset of The Left Graph</li>');
+					//	$(scores[id1]).find('.false').append('<li>None of The Right Graph vertices match those of The Left Graph</li>');
+					}
+				}
+			});
+		}
+	});
+};
+
+//second ajax query unnecessary if following a match edge length
+var compareEdges = function(id1, id2){
+	$.ajax({
+		type: 'GET',
+		url: getEdgeLabelsById(id1),
+		dataType: 'application/json',
+		success: function(r1){ 
+			//console.log('success');
+		},
+		error: function(e){
+			var ar_labels = JSON.parse(e.responseText).results;
+			//console.log("The Left Graph edge labels: ");
+			//console.log(ar_labels);
+			$.ajax({
+				type: 'GET',
+				url: getMatchingEdges(id2, ar_labels),
+				dataType: 'application/json',
+				success: function(r){ 
+					//console.log('success');
+				},
+				error: function(e){
+					var te_matches = JSON.parse(e.responseText).results;
+					//console.log("The Right Graph edge matches: ");
+					//console.log(te_matches);
+					if (te_matches.length === ar_labels.length){
+						$(scores[id1]).find('.true').append('<li>The Left Graph egdes are subset of The Right Graph</li>');
+					//} else if (te_matches.length > 0) {
+					//	$(scores[id1]).find('.true').append('<li>Some of The Left Graph egdes match those of The Right Graph</li>');
+					} else {
+						$(scores[id1]).find('.false').append('<li>The Left Graph egdes are not subset of The Right Graph</li>');
+					//	$(scores[id1]).find('.false').append('<li>None of The Left Graph egdes match those of The Right Graph</li>');
+					}
+				}
+			});
+		}
+	});
+	
+	$.ajax({
+		type: 'GET',
+		url: getEdgeLabelsById(id2),
+		dataType: 'application/json',
+		success: function(r1){ 
+			//console.log('success');
+		},
+		error: function(e){
+			var te_labels = JSON.parse(e.responseText).results;
+			//console.log("The Right Graph edge labels: ");
+			//console.log(te_labels);
+			$.ajax({
+				type: 'GET',
+				url: getMatchingEdges(id1, te_labels),
+				dataType: 'application/json',
+				success: function(r){ 
+					//console.log('success');
+				},
+				error: function(e){
+					var ar_matches = JSON.parse(e.responseText).results;
+					//console.log("The Left Graph edge matches: ");
+					//console.log(ar_matches);
+					if (ar_matches.length === te_labels.length){
+						$(scores[id1]).find('.true').append('<li>The Right Graph edges are subset of The Left Graph</li>');
+					//} else if (ar_matches.length > 0){
+					//	$(scores[id1]).find('.true').append('<li>Some of The Right Graph egdes match those of The Left Graph</li>');
+					} else {
+						$(scores[id1]).find('.false').append('<li>The Right Graph edges are not subset of The Left Graph</li>');
+					//	$(scores[id1]).find('.false').append('<li>None of The Right Graph egdes match those of The Left Graph</li>');
+					}
+				}
+			});
+		}
+	});
+};
+
+var compareOrientation = function(id1, id2){
+	$.ajax({
+		type: 'GET',
+		url: getAssertionsById(id1),
+		dataType: 'application/json',
+		success: function(r1){ 
+			//console.log('success');
+		},
+		error: function(e){
+			var assertions = JSON.parse(e.responseText).results;
+			
+			for (var i = 0; i < assertions.length; i++){
+				assertions[i] = assertions[i].slice(2, assertions[i].length);
+			}
+			//console.log("The Left Graph assertions: ");
+			//console.log(assertions);
+			
+			$.ajax({
+				type: 'GET',
+				url: getMatchingOrientation(id2, assertions),
+				dataType: 'application/json',
+				success: function(r){
+					//console.log('success');
+				},
+				error: function(e){
+					var te_matches = JSON.parse(e.responseText).results;
+					//console.log("The Right Graph matches: ");
+					//console.log(te_matches);
+					if (assertions.length === te_matches.length) {
+						$(scores[id1]).find('.true').append('<li>The Left Graph is a subset of The Right Graph</li>');
+					} else {
+						$(scores[id1]).find('.false').append('<li>The Left Graph is not a subset of The Right Graph</li>');
+					}
+				}
+			});
+		}
+	});
+	
+	$.ajax({
+		type: 'GET',
+		url: getAssertionsById(id2),
+		dataType: 'application/json',
+		success: function(r1){ 
+			//console.log('success');
+		},
+		error: function(e){
+			var assertions = JSON.parse(e.responseText).results;
+			
+			for (var i = 0; i < assertions.length; i++){
+				assertions[i] = assertions[i].slice(2, assertions[i].length);
+			}
+			
+			$.ajax({
+				type: 'GET',
+				url: getMatchingOrientation(id1, assertions),
+				dataType: 'application/json',
+				success: function(r){
+					//console.log('success');
+				},
+				error: function(e){
+					var ar_matches = JSON.parse(e.responseText).results;
+					
+					//console.log("The Left Graph matches: ");
+					//console.log(ar_matches);
+					if (assertions.length === ar_matches.length){
+						$(scores[id1]).find('.true').append('<li>The Right Graph is a subset of The Left Graph</li>');
+					} else {
+						$(scores[id1]).find('.false').append('<li>The Right Graph is not a subset of The Left Graph</li>');
+					}
+				}
+			});
+		}
+	});
+};
+
+var updateVertex = function(id, obj){
+	$.ajax({
+		type: 'POST',
+		url: updateVertexQuery(id, obj),
+		dataType: 'application/json',
+		success: function(r){
+			console.log('success');
+		},
+		error: function(e){
+			var data = JSON.parse(e.responseText).results;
+		}
+	});
+};
+
+var addScore = function(id){
+	scores[id] = $('<div>').appendTo('#information').attr('class', 'info').attr('id', id).hide();
+	$('<div>').appendTo(scores[id]).attr('class', 'true');
+	$('<div>').appendTo(scores[id]).attr('class', 'false');
+};
+
+var getScores = function(){
+	return scores;
+};
+
+var resetScores = function(){
+	$('.info').remove();
+	scores = {};
+	return scores;
+};
