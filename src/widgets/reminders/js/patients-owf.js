5800251dad090a0d13107d1ca115fa75b7ed6727
Rob Szewczyk <rob.szew@gmail.com>
2013-11-22 16:31:30 -0500

    removing console.log statements

--- src/widgets/reminders/js/patients-owf.js
+++ src/widgets/reminders/js/patients-owf.js
@@ -2,7 +2,6 @@ if(OWF.Util.isRunningInOWF()) {
 
     app.router.route('patient/:id', 'sendPatient', function(patientId) {
         var patient = app.patients.get(patientId);
-        console.log(patient.get('reminders'));
         var reminderEvents = [];
         var message = {};
         _.each(patient.get('reminders'), function(reminder) {
@@ -12,7 +11,6 @@ if(OWF.Util.isRunningInOWF()) {
         	});
         });
         message.events = reminderEvents;
-        console.log(message);
 
         OWF.ready(function () {
             OWF.Eventing.publish('com.nextcentury.everest.reminders.sendPatient', patient.toJSON());
