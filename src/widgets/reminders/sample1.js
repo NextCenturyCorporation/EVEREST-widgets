6aa0606be31c5948e3a80c93f97724e7de4375f2
Stephanie Schneider <stephanie.schneider@nextcentury.com>
2013-12-02 09:54:29 -0500

    un-minified jquery.timeline.js for debugging, added events to sample1

--- src/widgets/reminders/sample1.js
+++ src/widgets/reminders/sample1.js
@@ -4,7 +4,18 @@ app.eventData = { events:
 [
   {
     start: "Nov 14 2013 00:00:00 GMT",
-    title: "Some Event"
+    title: "Some Event",
+    end: "Nov 14 2013 00:00:00 GMT"
+  },
+  {
+  	start: "Nov 10 2013 00:00:00 GMT",
+  	title: "Some Event",
+  	end: "Nov 10 2013 00:00:00 GMT"
+  },
+  {
+  	start: "Oct 31 2013 00:00:00 GMT",
+  	title: "Some long event",
+  	end: "Nov 3 2013 00:00:00 GMT"
   }
 ]
 };
