733538146f61039b4f050ab8a9f7d38aad8fffc2
Andrew Curry <andrew.curry@nextcentury.com>
2013-12-04 18:19:33 -0500

    WIP - Need to separate heatChart-specific config from top-level Gruntfile

--- src/widgets/heatChart/js/main.js
+++ src/widgets/heatChart/js/main.js
@@ -1,7 +1,8 @@
 require.config({
     paths: {
         jquery: 'libs/jquery-2.0.3.min',
-        underscore: 'libs/underscore-1.5.2.min'
+        underscore: 'libs/underscore-1.5.2.min',
+        d3: 'libs/d3.v3.min.js'
     },
 
     shim: {
