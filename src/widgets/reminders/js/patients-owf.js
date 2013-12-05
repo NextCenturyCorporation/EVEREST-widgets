c8016b5c5fb2ca486ea92bd0236331dc69d442dc
Ashley <aviendha2281@gmail.com>
2013-12-05 17:07:16 -0500

    loading in cubism file for bob smith as example history

--- src/widgets/reminders/js/patients-owf.js
+++ src/widgets/reminders/js/patients-owf.js
@@ -1,14 +1,17 @@
 if(OWF.Util.isRunningInOWF()) {
 
     app.router.route('patient/:id', 'sendPatient', function(patientId) {
-        app.reminders.fetch({
-    		success: function(){
-    			var patient = app.patients.get(patientId);
-        		OWF.ready(function () {
-        			OWF.Eventing.publish('com.nextcentury.everest.reminders.sendPatient', patient.toJSON());
-        			//OWF.Eventing.publish('com.nextcentury.everest.storyLine.events', message);
-        		});
-    		}
-    	});
+		var patient = app.patients.get(patientId);
+		OWF.ready(function () {
+			OWF.Eventing.publish('com.nextcentury.everest.reminders.sendPatient', patient.toJSON());
+			//OWF.Eventing.publish('com.nextcentury.everest.storyLine.events', message);
+
+            if (patient.get('name') === 'Bob Smith') {
+                $.getJSON('cubism.js', function(json){
+                    console.log(json);
+                    OWF.Eventing.publish('com.nextcentury.everest.storyLine.events', json);
+                });
+            }
+		});
     });
 }
--- src/widgets/reminders/js/reminders-owf.js
+++ src/widgets/reminders/js/reminders-owf.js
@@ -5,25 +5,25 @@ if(OWF.Util.isRunningInOWF()) {
             app.patient.set(msg);
             app.showReminderList();
 	    
-	    var message = { events: [] };
-		_.each(app.patient.get('reminders'), function(reminder){
-			var rem = app.reminders.get(reminder.reminder_id);
-			var description = 'Performed bye: ' + reminder.performedBy;
+		    var message = { events: [] };
+			_.each(app.patient.get('reminders'), function(reminder){
+				var rem = app.reminders.get(reminder.reminder_id);
+				var description = 'Performed bye: ' + reminder.performedBy;
 
-			_.each(rem.get('components'), function(comp) {
-				description += '<hr />Who: ' + comp.who + '<br />What: ' + comp.what + '<br />When: ' + comp.when;
+				_.each(rem.get('components'), function(comp) {
+					description += '<hr />Who: ' + comp.who + '<br />What: ' + comp.what + '<br />When: ' + comp.when;
+				});
+				
+				message.events.push({
+					title: reminder.title,
+					dateCompleted: reminder.dateCompleted,
+					completed: reminder.completed,
+					dueDate: reminder.dueDate,
+					description: description
+				});
 			});
-			
-			message.events.push({
-				title: reminder.title,
-				dateCompleted: reminder.dateCompleted,
-				completed: reminder.completed,
-				dueDate: reminder.dueDate,
-				description: description
-			});
-		});
 
-	    OWF.Eventing.publish('com.nextcentury.everest.storyLine.events', message);
+		    OWF.Eventing.publish('com.nextcentury.everest.storyLine.events', message);
         });
 
         OWF.notifyWidgetReady();
