497d30e6f6b0d96c3386c7def8ead279009b649f
Ashley <aviendha2281@gmail.com>
2013-12-05 13:25:52 -0500

    seeing what this modal does in owf

--- src/widgets/reminders/js/patients.js
+++ src/widgets/reminders/js/patients.js
@@ -4,7 +4,7 @@ app.router = new Backbone.Router();
 
 $(document).ready(function() {
 
-    var rawPatientListTemplate     = "<div class='panel-heading'><h4>Patients</h4></div><div class='panel-body'><ul class='nav nav-pills nav-stacked'></ul></div>"
+    var rawPatientListTemplate     = "<div class='panel-heading'><h4>Patients<button class='btn btn-xs pull-right' data-toggle='modal' data-target='#myModal' id='help'>?</button></h4></div><div class='panel-body'><ul class='nav nav-pills nav-stacked'></ul></div>"
       , rawPatientSelectorTemplate = "<a href='#/patient/{{ _id }}'>{{ name }}</a>";
 
     app.patients.fetch({
