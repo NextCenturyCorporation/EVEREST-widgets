fa4aeba3af544bb3ca648a311217272de36b9569
Ashley Ryan <aviendha2281@gmail.com>
2013-12-05 16:52:00 -0500

    moving reminder to storyline announcing from patient widget to reminder widget

--- src/widgets/reminders/js/patients-owf.js
+++ src/widgets/reminders/js/patients-owf.js
@@ -4,26 +4,9 @@ if(OWF.Util.isRunningInOWF()) {
         app.reminders.fetch({
     		success: function(){
     			var patient = app.patients.get(patientId);
-        		var message = { events: []};
-        		_.each(patient.get('reminders'), function(reminder) {
-        			var rem = app.reminders.get(reminder.reminder_id);
-        			var description = 'Performed by: ' + reminder.performedBy;
-        			_.each(rem.get('components'), function(comp){
-            			description += '<br />Who: ' + comp.who + '<br />What: ' + comp.what + '<br />When: ' + comp.when + '<hr />';
-        			});
-
-        			message.events.push({
-        				title: reminder.title,
-    					dateCompleted: reminder.dateCompleted,
-    					completed: reminder.completed,
-    					dueDate: reminder.dueDate,
-            			description: description
-        			});
-        		});
-
         		OWF.ready(function () {
         			OWF.Eventing.publish('com.nextcentury.everest.reminders.sendPatient', patient.toJSON());
-        			OWF.Eventing.publish('com.nextcentury.everest.storyLine.events', message);
+        			//OWF.Eventing.publish('com.nextcentury.everest.storyLine.events', message);
         		});
     		}
     	});
