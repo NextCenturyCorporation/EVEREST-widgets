9ba647c7ce57618aae828588cf9dd397705beedd
Ashley <aviendha2281@gmail.com>
2013-12-19 12:58:57 -0500

    fixing another typo

--- src/widgets/reminders/js/patients-owf.js
+++ src/widgets/reminders/js/patients-owf.js
@@ -5,12 +5,11 @@ if(OWF.Util.isRunningInOWF()) {
 		OWF.ready(function () {
 			OWF.Eventing.publish('com.nextcentury.everest.reminders.sendPatient', patient.toJSON());
             if (patient.get('name') === 'Bob Smith') {
-		        data.eraseData = true;
-                OWF.Eventing.publish('com.nextcentury.everest.storyLine.events', JSON.stringify(data);
-            } else {
-            	OWF.Eventing.publish('com.nextcentury.everest.storyLine.events', JSON.stringify({eraseData: true}));
+				data.eraseData = true;
+				OWF.Eventing.publish('com.nextcentury.everest.storyLine.events', JSON.stringify(data));
+			} else {
+				OWF.Eventing.publish('com.nextcentury.everest.storyLine.events', JSON.stringify({eraseData: true}));
             }
-
 		});
     });
 }
