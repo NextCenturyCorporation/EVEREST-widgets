bc7a1100166c5ebff78e6eeed3151c436a2ea5db
Ashley <aviendha2281@gmail.com>
2014-01-07 17:44:55 -0500

    fixing a typo in previous edit

--- src/widgets/reminders/js/patients-owf.js
+++ src/widgets/reminders/js/patients-owf.js
@@ -4,7 +4,7 @@ if(OWF.Util.isRunningInOWF()) {
 		var patient = app.patients.get(patientId);
 		OWF.ready(function () {
 			OWF.Eventing.publish('com.nextcentury.everest.reminders.sendPatient', patient.toJSON());
-			var configs: { eraseData: true, setDeduceLayout: false };
+			var configs = { eraseData: true, setDeduceLayout: false };
             if (patient.get('name') === 'Bob Smith') {
 				data.configs = configs;
 				OWF.Eventing.publish('com.nextcentury.everest.storyLine.events', JSON.stringify(data));
