1babf4f53ff856ec7370a016fc1e799ee9f9e02b
Ashley <aviendha2281@gmail.com>
2014-01-07 13:44:22 -0500

    updating tagCloud to scale font sizes better

--- src/widgets/tagCloud/js/tags.js
+++ src/widgets/tagCloud/js/tags.js
@@ -1,12 +1,38 @@
-var url = 'http://everest-build:8081/tag?sortKey=value.count&sort=desc&count=200'
+var url = 'http://everest-build:8081/tag?sortKey=value.count&sort=desc&count=300'
 var a,b;
+
+var getFontSize = function(x1, x2, value){
+	var y1 = 100;
+	var y2 = 20;
+
+	if (x1 === x2) {
+		return value;
+	}
+
+	var c1 = Math.log(y1/y2)/(x1 - x2);
+	var c2 = y1 / Math.exp(c1 * x1);
+
+	return c2 * Math.exp(value * c1)
+};
+
+var announce = function(str) {
+	if(OWF.Util.isRunningInOWF()) {
+
+	    OWF.ready(function() {
+	        OWF.Eventing.publish('com.nextcentury.everest.tagCloud', str);
+	    });
+	}
+}
+
 $(function(){
 	$.ajax({
 		url: url,
 		type: 'GET',
-		success: function(data){
+		success: function(data) {
+			var firstCount = data.docs[0].value.count;
+			var lastCount = data.docs[data.docs.length - 1].value.count;
 			a = data.docs.map(function(d){
-				return { text: d._id, count: Math.log(d.value.count) };
+				return { text: d._id, count: getFontSize(firstCount, lastCount, d.value.count) };
 			});
 
 			b = data.docs;
@@ -14,18 +40,16 @@ $(function(){
 		}
 	});
 
-	var createCloud = function(data){
+	var createCloud = function(data) {
 		var w = 960;
 		var h = 600;
-		var fill = d3.scale.category20();
+		var fill = d3.scale.category20b();
 
 		d3.layout.cloud().size([w, h])
 			.words(data)
 			.rotate(function() { return ~~(Math.random() * 2) * 90; })
 			.font('Impact')
-			.fontSize(function(d) { 
-				return d.count * 20 - 100; 
-			})
+			.fontSize(function(d) { return d.count; })
 			.on('end', draw)
 			.start();
 
@@ -39,7 +63,7 @@ $(function(){
 					.data(words)
 					.enter().append('text')
 						.style('font-size', function(d) { 
-							return (d.count * 20 - 100) + 'px'; 
+							return d.count + 'px'; 
 						})
 						.style('font-family', 'Impact')
 						.style('fill', function(d, i) { return fill(i); })
@@ -50,7 +74,8 @@ $(function(){
 						.text(function(d) { return d.text; })
 						.on('click', function(){
 							var ind = b.map(function(e){return e._id}).indexOf(d3.select(this).text());
-							console.log(b[ind].value.reports)
+							console.log(b[ind].value.reports);
+							announce(b[ind].value.reports)
 						})
 		}
 	}
